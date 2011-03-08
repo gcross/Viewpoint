84d8be646065f5480cafb5543bdc726a4764c3e7
Gregory Crosswhite <gcross@phys.washington.edu>
2011-03-08 12:45:06 -0800

    Added "-cp ." to test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 scalac Viewpoint.scala && (
     scala org.scalatest.tools.Runner -p . -o -s Viewpoint.Testing.ParserSpecification;
-    scala Viewpoint.Testing.ParserSpecification
+    scala -cp . Viewpoint.Testing.ParserSpecification
 )
