84d00bca1565610859ab216c24fa9d9484f47e0a
gcross <gcross@phys.washington.edu>
2011-04-15 11:05:22 -0700

    Wrote TreeChangeAdapter to make it easier to write TreeChangeListeners.

---
+++ src/main/java/viewpoint/event/TreeChangeAdapter.java
@@ -0,0 +1,15 @@
+//@+leo-ver=5-thin
+//@+node:gcross.20110414153139.1545: * @file TreeChangeAdapter.java
+//@@language Java
+package viewpoint.event;
+
+public class TreeChangeAdapter implements TreeChangeListener {
+
+    public void treeNodeBodyChanged(NodeBodyChangedEvent event) {}
+    public void treeNodeChildInserted(ChildInsertedEvent event) {}
+    public void treeNodeChildRemoved(ChildRemovedEvent event) {}
+    public void treeNodeHeadingChanged(NodeHeadingChangedEvent event) {}
+    public void treeNodeStructureChanged(StructureChangedEvent event) {}
+
+}
+//@-leo
