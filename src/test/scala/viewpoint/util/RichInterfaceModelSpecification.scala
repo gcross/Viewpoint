d24fc720ae7e499ca78e51bf29e7aa7e5c79d9c4
gcross <gcross@phys.washington.edu>
2011-04-22 21:51:34 -0700

    Ported the comparison tests from NodeSpecification to RichInterfaceSpecification

--- src/test/scala/viewpoint/util/RichInterfaceModelSpecification.scala
+++ src/test/scala/viewpoint/util/RichInterfaceModelSpecification.scala
@@ -16,6 +16,167 @@ import viewpoint.util.RichInterface._
 //@+node:gcross.20110422115402.2032: ** RichInterfaceModelSpecification
 abstract class RichInterfaceModelSpecification(createEmptyTree: => Tree) extends Spec with ShouldMatchers {
   //@+others
+  //@+node:gcross.20110422202916.1655: *3* The === method gets the correct answer for
+  describe("The === method gets the correct answer for") {
+    //@+others
+    //@+node:gcross.20110422202916.1656: *4* identical singletons.
+    it("identical singletons.") {
+      val node1 = createEmptyTree.createNode("id","heading","body")
+      val node2 = createEmptyTree.createNode("id","heading","body")
+      (node1 === node2) should be (true)
+    }
+    //@+node:gcross.20110422204808.1656: *4* singletons with different ids.
+    it("singletons with different ids.") {
+      val node1 = createEmptyTree.createNode("id1","heading","body")
+      val node2 = createEmptyTree.createNode("id2","heading","body")
+      (node1 === node2) should be (false)
+    }
+    //@+node:gcross.20110422204808.1658: *4* singletons with different headings.
+    it("singletons with different headings.") {
+      val node1 = createEmptyTree.createNode("id","heading1","body")
+      val node2 = createEmptyTree.createNode("id","heading2","body")
+      (node1 === node2) should be (false)
+    }
+    //@+node:gcross.20110422204808.1660: *4* singletons with different bodies.
+    it("singletons with different bodies.") {
+      val node1 = createEmptyTree.createNode("id","heading","body1")
+      val node2 = createEmptyTree.createNode("id","heading","body2")
+      (node1 === node2) should be (false)
+    }
+    //@+node:gcross.20110422204808.1661: *4* identical nodes with identical children.
+    it("identical nodes with identical children.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      tree1.appendChildrenTo(node1,
+        tree1.createNode("ida","heading","body"),
+        tree1.createNode("idb","heading","body")
+      )
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      tree2.appendChildrenTo(node2,
+        tree2.createNode("ida","heading","body"),
+        tree2.createNode("idb","heading","body")
+      )
+      (node1 === node2) should be (true)
+    }
+    //@+node:gcross.20110422204808.1663: *4* identical nodes with different children.
+    it("identical nodes with different children.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      tree1.appendChildrenTo(node1,
+        tree1.createNode("ida","heading1","body"),
+        tree1.createNode("idb","heading2","body")
+      )
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      tree2.appendChildrenTo(node2,
+        tree2.createNode("ida","heading2","body"),
+        tree2.createNode("idb","heading1","body")
+      )
+      (node1 === node2) should be (false)
+    }
+    //@+node:gcross.20110422204808.1665: *4* identical nodes with identical clone children.
+    it("identical nodes with identical clone children.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      val node1a = tree1.createNode("ida","heading","body")
+      tree1.appendChildrenTo(node1,node1a,node1a)
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      val node2a = tree2.createNode("ida","heading","body")
+      tree2.appendChildrenTo(node2,node2a,node2a)
+      (node1 === node2) should be (true)
+    }
+    //@+node:gcross.20110422204808.1668: *4* identical nodes with different clone children.
+    it("identical nodes with different clone children.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      val node1a = tree1.createNode("ida","heading1","body")
+      tree1.appendChildrenTo(node1,node1a,node1a)
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      val node2a = tree2.createNode("ida","heading2","body")
+      tree2.appendChildrenTo(node2,node2a,node2a)
+      (node1 === node2) should be (false)
+    }
+    //@+node:gcross.20110422204808.1670: *4* identical cyclic nodes.
+    it("identical cyclic nodes.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      tree1.appendChildrenTo(node1,
+        tree1.createNode("ida","heading","body"),
+        node1
+      )
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      tree2.appendChildrenTo(node2,
+        tree2.createNode("ida","heading","body"),
+        node2
+      )
+      (node1 === node2) should be (true)
+    }
+    //@+node:gcross.20110422204808.1672: *4* identical multiply nested nodes.
+    it("identical multiply nested nodes.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      val node1a = tree1.createNode("ida","heading","a")
+      val node1b = tree1.createNode("idb","heading","b")
+      val node1b1 = tree1.createNode("idb1","heading","b1")
+      val node1b1a = tree1.createNode("idb1a","heading","b1a")
+      val node1b1b = tree1.createNode("idb1b","heading","b1b")
+      val node1b2 = tree1.createNode("idb2","heading","b2")
+      tree1.appendChildrenTo(node1,node1a,node1b)
+      tree1.appendChildrenTo(node1a,node1b1)
+      tree1.appendChildrenTo(node1b,node1b1,node1b2,node1a)
+      tree1.appendChildrenTo(node1b1,node1b1a,node1b1b)
+
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      val node2a = tree2.createNode("ida","heading","a")
+      val node2b = tree2.createNode("idb","heading","b")
+      val node2b1 = tree2.createNode("idb1","heading","b1")
+      val node2b1a = tree2.createNode("idb1a","heading","b1a")
+      val node2b1b = tree2.createNode("idb1b","heading","b1b")
+      val node2b2 = tree2.createNode("idb2","heading","b2")
+      tree2.appendChildrenTo(node2,node2a,node2b)
+      tree2.appendChildrenTo(node2a,node2b1)
+      tree2.appendChildrenTo(node2b,node2b1,node2b2,node2a)
+      tree2.appendChildrenTo(node2b1,node2b1a,node2b1b)
+
+      (node1 === node2) should be (true)
+    }
+    //@+node:gcross.20110422204808.1674: *4* different multiply nested nodes.
+    it("different multiply nested nodes.") {
+      val tree1 = createEmptyTree
+      val node1 = tree1.createNode("id","heading","body")
+      val node1a = tree1.createNode("ida","heading","a")
+      val node1b = tree1.createNode("idb","heading","b")
+      val node1b1 = tree1.createNode("idb1","heading","b1")
+      val node1b1a = tree1.createNode("idb1a","heading","b1a")
+      val node1b1b = tree1.createNode("idb1b","heading","b1b")
+      val node1b2 = tree1.createNode("idb2","heading","b2")
+      tree1.appendChildrenTo(node1,node1a,node1b)
+      tree1.appendChildrenTo(node1a,node1b1)
+      tree1.appendChildrenTo(node1b,node1b1,node1b2,node1a)
+      tree1.appendChildrenTo(node1b1,node1b1a,node1b1b)
+
+      val tree2 = createEmptyTree
+      val node2 = tree2.createNode("id","heading","body")
+      val node2a = tree2.createNode("ida","heading","a")
+      val node2b = tree2.createNode("idb","heading","b")
+      val node2b1 = tree2.createNode("idb1","heading","b1")
+      val node2b1a = tree2.createNode("idb1a","heading","b1b")
+      val node2b1b = tree2.createNode("idb1b","heading","b1b")
+      val node2b2 = tree2.createNode("idb2","heading","b2")
+      tree2.appendChildrenTo(node2,node2a,node2b)
+      tree2.appendChildrenTo(node2a,node2b1)
+      tree2.appendChildrenTo(node2b,node2b1,node2b2,node2a)
+      tree2.appendChildrenTo(node2b1,node2b1a,node2b1b)
+
+      (node1 === node2) should be (false)
+    }
+    //@-others
+  }
   //@+node:gcross.20110422115402.2033: *3* The getChildren method iterates correctly over children.
   describe("The getChildren method iterates correctly over children.") {
     val tree = createEmptyTree
